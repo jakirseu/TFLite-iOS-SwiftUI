6337b2e9be3d03d0ef313254ac90223665d11d46
Jakir Hossain <jakirseu@gmail.com>
2023-10-16 01:47:45 +0600

    Initial Commit

---
+++ TFLiteExample/TFLiteExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFLiteExampleApp.swift
+//  TFLiteExample
+//
+//  Created by Jakir Hossain on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TFLiteExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
